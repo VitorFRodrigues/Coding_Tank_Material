5e52fb2a7d1c0ed21fdcf762c35031d0d3a7a309
Vitor Fernando de Souza Rodrigues <vitorfbaiano@gmail.com>
2022-12-26 11:10:18 -0300

    Atualização Pasta

--- Polo_Tech/Modulo_02/Aula01.sql
+++ Polo_Tech/Modulo_02/Aula01.sql
@@ -1,19 +1,17 @@
-create table professores(
-	professor_id SERIAL not null,
+CREATE TABLE professores(
+	professor_id SERIAL NOT NULL,
 	nome VARCHAR(40),
-	dominio1 varchar(40),
-	dominio2 varchar(40),
-	dominio3 varchar(40)
+	dominio1 VARCHAR(40),
+	dominio2 VARCHAR(40),
+	dominio3 VARCHAR(40)
 	
 );
 
-insert into professores (nome, 
+INSERT INTO professores (nome, 
 						dominio1, 
 						dominio2, 
-						dominio3) 
-						values (
-								'Sandro Saorin',
-								'SQL',
-								'Machine Learning',
-								'Data Science');
-								
+						dominio3)
+			VALUES ('Sandro Saorin',
+					'SQL',
+					'Machine Learning',
+					'Data Science');
---
+++ Polo_Tech/Modulo_02/Aula03_Lista_de_Exercicios.sql
@@ -0,0 +1,106 @@
+-- Base de dados - sales
+
+-- Exercicio 1: Quantos itens distintos foram vendidos (item_id)?
+SELECT 
+	COUNT(DISTINCT item_id) AS itens_distintos
+FROM 
+	sales;
+
+-- Exercicio 2: Quantas lojas existem na base (loja_id)?
+SELECT 
+	COUNT(DISTINCT loja_id) AS itens_distintos
+FROM 
+	sales;
+
+-- Exercicio 3: Qual é a cidade que mais tem lojas (loja_id)?
+SELECT 
+	DISTINCT cidade,
+	COUNT(DISTINCT loja_id) AS qtd_lojas
+FROM
+	sales
+GROUP BY DISTINCT cidade
+ORDER BY COUNT(DISTINCT loja_id) DESC
+LIMIT 1;
+
+-- Exercicio 4: Em qual municipio foi vendido mais itens?
+SELECT 
+	DISTINCT municipio,
+	COUNT(item_id) AS itens_vendidos
+FROM
+	sales
+GROUP BY DISTINCT municipio
+ORDER BY COUNT(item_id) DESC
+LIMIT 1;
+
+-- Exercicio 5: Qual a cidade mais vendeu em dolares?
+SELECT 
+	DISTINCT cidade,
+	SUM(total_venda) AS total_venda_cidade
+FROM
+	sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+-- Exercicio 6: Qual foi o dia que mais vendeu itens distintos?
+SELECT 
+	DISTINCT data_pedido,
+	COUNT(DISTINCT item_id) AS total_itens_id
+FROM
+	sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1;
+
+-- Exercicio 7: Monte o top 5 bebidas (item_id) mais vendidas por valor?
+SELECT 
+	DISTINCT item_id,
+	SUM(total_venda) AS total_venda_item_id
+FROM
+	sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
+
+-- Exercicio 8: Monte o top 5 bebidas (item_id) mais vendidas por volume em litros
+SELECT 
+	DISTINCT item_id,
+	SUM(volume_litros_vendidos) AS total_venda_litros
+FROM
+	sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 5;
+
+-- Exercicio 9: Quais são as 10 empresas (marca_id) que mais lucram com a venda de bebidas?
+SELECT 
+	DISTINCT marca_id,
+	SUM(total_venda - custo_garrafa*garrafas_vendidas) AS lucro_marca
+FROM 
+	sales
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- Exercicio 10: Monte uma tabela com a quantidade de vendas distintas por dia.
+SELECT 
+	DISTINCT data_pedido,
+	COUNT(DISTINCT item_id) AS qtd_venda_distinta
+FROM 
+	sales
+GROUP BY 1
+ORDER BY 2 DESC 
+
+
+SELECT * 
+FROM 
+	sales
+ORDER BY total_venda
+LIMIT 5;
+
+SELECT 
+	DISTINCT item_id,
+	COUNT(item_id)
+FROM
+	sales
+GROUP BY 1
